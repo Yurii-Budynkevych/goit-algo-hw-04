3ace72a096cbf7de3ffc2ca7cf734bee1d0b0acb
Yurii Budynkevych <yurii.budynkevych@gmail.com>
2025-05-23 19:00:42 +0200

    all lelevs included

--- visualise.py
+++ visualise.py
@@ -1,19 +1,33 @@
 import sys
 import pathlib
-from colorama import Fore, Style
+from colorama import Fore, Style, init
 
-try:
-    dir_path = pathlib.Path(sys.argv[1])
-    if dir_path.exists():
-        if dir_path.is_dir():
-            items = dir_path.iterdir()
-            for item in items:
-                print(Fore.GREEN + str(item))
+init(autoreset=True)  # Automatically reset color after each print
+
+def list_directory(path: pathlib.Path, level=0):
+    indent = "  " * level  # Indent based on level of depth
+
+    try:
+        for item in path.iterdir():
+            if item.is_dir():
+                print(f"{indent}{Fore.BLUE}{item.name}/")
+                list_directory(item, level + 1)  # Recursive call
+            else:
+                print(f"{indent}{Fore.GREEN}{item.name}")
+    except PermissionError:
+        print(f"{indent}{Fore.RED}[Permission Denied] {item.name}")
+
+def main():
+    try:
+        dir_path = pathlib.Path(sys.argv[1])
+        if not dir_path.exists():
+            print(Fore.RED + "Path not found")
+        elif not dir_path.is_dir():
+            print(Fore.RED + "Given path is not a directory")
         else:
-            print(Fore.RED + 'can not list file, give dir')    
-    else:
-        print(Fore.RED + 'not found')
-except IndexError as err:
-    print(Fore.RED + f'{err}, plese write a path')
+            list_directory(dir_path)
+    except IndexError:
+        print(Fore.RED + "No path provided. Please provide a directory path as an argument.")
 
-Style.RESET_ALL        
+if __name__ == '__main__':
+    main()
